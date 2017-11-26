a454d147c44c6c90dcc4f68da8d2f096cf015411
Mustapha <Mustapha.boulouh@icloud.com>
2017-11-26 02:28:54 +0100

    PS-2017.2.4 <Mustapha@Mustapha Overwrite remote C:\xampp\htdocs\Egestion to local

--- app/Views/w_errors/404.php
+++
@@ -1,6 +0,0 @@
-<?php $this->layout('layout', ['title' => 'Perdu ?']) ?>
-
-<?php $this->start('main_content'); ?>
-<div class="ERREUR" id="animationWindow">
-</div>
-<?php $this->stop('main_content'); ?>
